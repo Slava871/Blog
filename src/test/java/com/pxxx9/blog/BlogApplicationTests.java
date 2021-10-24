38ec0d4c3f1561aef88d4dd3d7a6578c7d3701df
Slava <loginmail2018@inbox.ru>
2021-10-24 21:04:01 +0300

    unnesisary test file

---
+++ src/test/java/com/pxxx9/blog/BlogApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pxxx9.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
